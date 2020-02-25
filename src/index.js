22503f2506bfc6ab1c07adba85c254c0562b7198
Polashkind <polashkind@gmail.com>
2020-02-26 02:07:05 +0300

    feat: implement task for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    };
+    
+    let s = array[0];
 
-exports.min = function min (array) {
-  return 0;
+    for (i = 0; i < array.length; i++) {
+        if (s > array[i]) {
+            s = array[i];
+        }
+    }
+
+    return s;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    };
+
+    let k = array[0];
+
+    for (i = 0; i < array.length; i++) {
+        if (k < array[i]) {
+            k = array[i];
+        }
+    }
+
+    return k;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    };
+
+    let total = 0;
+
+    for (i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+
+    return total / array.length;
 }
